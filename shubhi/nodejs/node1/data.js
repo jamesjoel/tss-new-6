f43539dc91b234613c0a0daa9131ed4ed2f32722
Shubhisahu <shubhisahu272@gamil.com>
2025-11-04 17:09:43 +0530

    hello

---
+++ shubhi/nodejs/node1/data.js
@@ -0,0 +1,11 @@
+let Products = [
+    { id: 1, name: 'Laptop', price: 1000 },
+    { id: 2, name: 'Phone', price: 500 },
+    { id: 3, name: 'Tablet', price: 300 }
+];
+let Employees = [
+    { id: 1, name: 'Alice', position: 'Manager' },
+    { id: 2, name: 'Bob', position: 'Developer' },
+    { id: 3, name: 'Charlie', position: 'Designer' }
+];
+export { Products, Employees };
