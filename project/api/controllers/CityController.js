f379ea027cdac41a79a269af1e8171b66ba1cb7f
jamesjoel <james.joelj@gmail.com>
2026-02-11 16:51:01 +0530

    razorpay-1

--- project/api/controllers/CityController.js
+++ project/api/controllers/CityController.js
@@ -1,6 +1,16 @@
 import City from "../models/City.js";
+import Razorpay from 'razorpay'
 
 import DoSendMail from '../helpers/SendMailHelper.js';
+
+const RAZORPAY_KEY = "rzp_test_Rek8z2OtrReaiV";
+const RAZORPAY_SECRET = "c3QZBCHSmaMmuLK9Gc9y1MtK";
+
+let rzpy = new Razorpay({
+    key_id : RAZORPAY_KEY,
+    key_secret : RAZORPAY_SECRET
+})
+
 
 let GetAllCity = async(req, res)=>{
     let result = await City.find();
@@ -11,4 +21,17 @@ let SendMail = async(req, res)=>{
     res.send({success:true});
 }
 
-export {GetAllCity, SendMail}
+let DoPayment = async(req, res)=>{
+    let amount = 100;
+    try{
+        const order = await rzpy.orders.create({
+            amount : amount*100,
+            currency : 'INR'
+        });
+        res.send({success:true, orderId : order.id})
+    }catch(err){
+        res.send({success:false})
+    }
+}
+
+export {GetAllCity, SendMail, DoPayment}
--- project/api/routes/CityRoutes.js
+++ project/api/routes/CityRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { GetAllCity, SendMail } from '../controllers/CityController.js';
+import { DoPayment, GetAllCity, SendMail } from '../controllers/CityController.js';
 let routes = express.Router();
 
 routes.get("/", GetAllCity);
 routes.get("/sendmail", SendMail)
+routes.get("/payment", DoPayment)
 
 export default routes;
